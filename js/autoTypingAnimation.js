57453da54378b4a10e7ab35bed373318aa2b70d6
KetanKumar21 <123578051+KetanKumar21@users.noreply.github.com>
2023-01-31 02:54:59 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Ketan Kumar. I hail from Begusarai, Bihar currently persuing my B. Tech degree in Computer Science Engineering - final year, at Vellore Institute of Technology Bhopal University"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
